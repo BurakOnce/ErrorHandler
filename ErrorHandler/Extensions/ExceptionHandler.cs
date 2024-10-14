483fc59c0110d516d2e8d1e0ea32285bed85aff5
burakonce9 <burakonce9@gmail.com>
2024-10-14 16:05:46 +0300

    Add project files.

---
+++ ErrorHandler/Extensions/ExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ErrorHandler.Extensions
+{
+    public class ExceptionHandler
+    {
+        private readonly RequestDelegate _delegate;
+        public ExceptionHandler(RequestDelegate @delegate)
+        {
+            _delegate = @delegate;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _delegate.Invoke(context);
+                if (context.Response.StatusCode == (int)HttpStatusCode.NotFound)
+                {
+                    context.Response.Redirect("/Home/Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                context.Response.Redirect("/Home/Error");
+            }
+
+        }
+    }
+}
